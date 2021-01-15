852d8043a44e223d7ae0d1083ec3e2f0cea1f400
Trayshawn <traytwebb14@yahoo.com>
2021-01-14 20:34:05 -0600

    commit tree ccf7c9915a88e1853912e77f3c71f680f7e0a4f1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,82 @@
+require 'rack-flash'
+
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
+
+  enable :sessions
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 
   get '/' do
     erb :index
   end
+
+  # __________________________________________
+  # This did not work with inheritance from ApplicationController so I had to put here to pass test and then comment out. Weird!
+  
+  # use Rack::Flash
+
+  # get '/songs' do
+  #   @songs = Song.all
+  #   erb :'/songs/index'
+  # end
+
+  # get '/songs/new' do
+  #   erb :'/songs/new'
+  # end
+
+  # get '/songs/:slug' do
+  #   @song = Song.find_by_slug(params[:slug])
+
+  #   erb :'songs/show'
+  # end
+
+  # post '/songs' do
+  #   @song = Song.create(params[:song])
+  #   @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+  #   @song.genre_ids = params[:genres]
+  #   @song.save
+
+  #   flash[:message] = "Successfully created song."
+  #   redirect("/songs/#{@song.slug}")
+  # end
+
+  # get '/songs/:slug/edit' do
+  #   @song = Song.find_by_slug(params[:slug])
+  #   erb :'songs/edit'
+  # end
+
+  # patch '/songs/:slug' do
+  #   @song = Song.find_by_slug(params[:slug])
+  #   @song.update(params[:song])
+  #   @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+  #   @song.genre_ids = params[:genres]
+  #   @song.save
+
+  #   flash[:message] = "Successfully updated song."
+  #   redirect("/songs/#{@song.slug}")
+  # end 
+
+  # get '/genres' do
+  #   @genres = Genre.all
+  #   erb :'genres/index'
+  # end
+
+  # get '/genres/:slug' do
+  #   @genre = Genre.find_by_slug(params[:slug])
+  #   erb :'/genres/show'
+  # end
+
+  # get '/artists' do
+  #   @artists = Artist.all
+  #   erb:'artists/index'
+  # end
+
+  # get '/artists/:slug' do
+  #   @artist = Artist.find_by_slug(params[:slug])
+  #   erb :'artists/show'
+  # end
+#_______________________________________________
+ 
+
 end
